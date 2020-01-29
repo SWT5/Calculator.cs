f8df30dd48afca69f3de1668ae1bfc7a21c43cb5
Thanh Tieu Pham <au479218@uni.au.dk>
2020-01-29 15:54:23 +0100

    added calculator

---
+++ Calculator.Test.Unit/CalculatorTest.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+
+namespace Calculator.Test.Unit
+{
+    [TestFixture]
+    public class CalculatorTest
+    {
+        private Calculator.cs.Calculator uut;
+
+        [SetUp]
+        public void Setup()
+        {
+            uut = new Calculator.cs.Calculator();
+        }
+
+        [Test]
+        public void addNumbers_twoNumbers_return1()
+        {
+            //arrange
+            //Act
+            //Assert
+            Assert.That(uut.Add(1,0), Is.EqualTo(1));
+        }
+        
+        [Test]
+        public void addNumbers_twoNumbers_secondMinusFirstPlus_return1()
+        {
+
+            Assert.That(uut.Add(2,-1), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void addNumbers_twoNumbers_FirstMinusSecondPlus_return1()
+        {
+
+            Assert.That(uut.Add(-1, 2), Is.EqualTo(1));
+        }
+        
+        [Test]
+        public void subractNumbers_twoNumbers_return1()
+        {
+            Assert.That(uut.Subtract(2,1),Is.EqualTo(1));
+        }
+
+        [Test]
+        public void subtractNumbers_twoNumbers_FirstMinusSecondPlus_return3()
+        {
+            Assert.That(uut.Subtract(-2, 1), Is.EqualTo(-3));
+        }
+
+        [Test]
+        public void subtractNumbers_twoNumbers_secondMinusFirstPlus_return3()
+        {
+            Assert.That(uut.Subtract(1, -2), Is.EqualTo(3));
+        }
+
+        [Test]
+        public void multiplyNumbers_twoNumbers_return1()
+        {
+            Assert.That(uut.Multiply(1,1), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void multiplyNumbers_twoNumbers_FirstMinusSecondPlus_returnMinus1()
+        {
+            Assert.That(uut.Multiply(-1, 1), Is.EqualTo(-1));
+        }
+
+        [Test]
+        public void multiplyNumbers_twoNumbers_secondMinusFirstPlus_returnMinus1()
+        {
+            Assert.That(uut.Multiply(1, -1), Is.EqualTo(-1));
+        }
+
+        [Test]
+        public void powerNumbers_twoNumbers_return1()
+        {
+            Assert.That(uut.Power(1, 1), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void powerNumbers_twoNumbers_FirstMinusSecondPlus_returnMinus1()
+        {
+            Assert.That(uut.Power(-1, 1), Is.EqualTo(-1));
+        }
+
+        [Test]
+        public void powerNumbers_twoNumbers_secondMinusFirstPlus_returnMinus1()
+        {
+            Assert.That(uut.Power(1, -1), Is.EqualTo(1));
+        }
+    }
+}
---
+++ Calculator.cs/Calculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.cs
+{
+    public class Calculator : ICalculator
+    {
+        public double get_total { get; }
+        private double total_;
+        public Calculator()
+        {
+            total_ = 0;
+        }
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double Power(double x, double exp)
+        {
+            return Math.Pow(x, exp);
+        }
+    }
+}
