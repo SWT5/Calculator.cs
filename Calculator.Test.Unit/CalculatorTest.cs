095951539e569c09bcbf6c9547d8f6c7ada96823
Nikolaj Huynh <201706357@post.au.dk>
2020-02-05 11:40:39 +0100

    random commit

--- Calculator.Test.Unit/CalculatorTest.cs
+++ Calculator.Test.Unit/CalculatorTest.cs
@@ -213,10 +213,9 @@ namespace Calculator.Test.Unit
         [TestCase(-4,5, -1024)]
         [TestCase(5, -4, 0.0016)]
         [TestCase(-5, -4, 0.0016)]
-        [TestCase(-5, -4, 10)]
         public void pow_ExpChar(double a,double b, double result)
         {
-            Assert.That(uut.Power(a, b), Is.EqualTo(result));
+            Assert.That(uut.Power(a, b), Is.EqualTo(result).Within(0.05));
         }
 
         /* division*/
